3329c6f57a5da2525c0364a0e6b2f13917feadac
Nguyễn Lâm <nctl144@gmail.com>
2017-03-07 00:37:27 -0500

    Add masonry to about us page

--- page-templates/page_about.php
+++ page-templates/page_about.php
@@ -13,38 +13,41 @@ get_header(); ?>
 					<?php the_content(); ?>
 				<?php endwhile; ?>
 			</div><!-- .entry-content -->
-			<div class="grid-container">
-			<?php
-				$args = array(
-					'orderby'          => 'name',
-				    'order'            => 'ASC',
-					'post_type'        => 'SAteam',
-					'posts_per_page'   => -1,
-				);
 
-				$postslist = new WP_Query($args);
+			<div id="masonry-grid">
+				<div class="grid-container">
+				<?php
+					$args = array(
+						'orderby'          => 'name',
+					    'order'            => 'ASC',
+						'post_type'        => 'SAteam',
+						'posts_per_page'   => -1,
+					);
 
-				if ($postslist->have_posts()):
-				    while ($postslist->have_posts()):
-				    	$postslist->the_post(); 
-				    ?>
-				    <article class="aboutus-post-wrapper grid-25 tablet-grid-33 mobile-grid-50">
-						<div class="aboutus-inner">
-							<div class="aboutus-thumbnail">
-								<?php the_post_thumbnail('create_thumb_small'); ?>
-							</div>
-							<div class="club-description">
-								<h3 class="aboutus-title"><?php the_title(); ?></h3>
-								<div class="aboutus-description-inner hidden">
-									<p> <?php the_excerpt(); ?> </p>
+					$postslist = new WP_Query($args);
+
+					if ($postslist->have_posts()):
+					    while ($postslist->have_posts()):
+					    	$postslist->the_post(); 
+					    ?>
+					    <article class="grid-item aboutus-post-wrapper grid-25 tablet-grid-33 mobile-grid-50">
+							<div class="aboutus-inner">
+								<div class="aboutus-thumbnail">
+									<?php the_post_thumbnail('create_thumb_small'); ?>
+								</div>
+								<div class="club-description">
+									<h3 class="aboutus-title"><?php the_title(); ?></h3>
+									<div class="aboutus-description-inner hidden">
+										<p> <?php the_excerpt(); ?> </p>
+									</div>
 								</div>
 							</div>
-						</div>
-					</article>
-					<?php
-				    endwhile;
-				endif;
-			?>
+						</article>
+						<?php
+					    endwhile;
+					endif;
+				?>
+				</div>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
