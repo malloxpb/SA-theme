bb04d93e4b9b7b7a172a83ed50ff3a39a0fb2dd7
Nguyễn Lâm <nctl144@gmail.com>
2017-03-20 19:40:56 -0400

    Move images to static folder

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -24,7 +24,7 @@
  */
 function sydney_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'sydney_custom_header_args', array(
-		'default-image'          => get_template_directory_uri() . '/images/header.jpg',
+		'default-image'          => get_template_directory_uri() . '/static/images/header.jpg',
 		'default-text-color'     => '000000',
 		'width'                  => 1920,
 		'height'                 => 1080,
