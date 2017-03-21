63858449db513d4b6bcf51855395605ac00a95a6
Nguyễn Lâm <nctl144@gmail.com>
2017-03-20 20:52:01 -0400

    delete the facts widget

--- inc/page-builder.php
+++ inc/page-builder.php
@@ -18,7 +18,6 @@ function sydney_theme_widgets($widgets) {
 		'Sydney_Services_Type_A',
 		'Sydney_Services_Type_B',
 		'Sydney_List',
-		'Sydney_Facts',
 		'Sydney_Testimonials',
 		'Sydney_Skills',
 		'Sydney_Video_Widget',
