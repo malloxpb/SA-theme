99fe9f6dcf2f1e2f1d57501d4d8f651999268a93
Nguyễn Lâm <nctl144@gmail.com>
2017-03-23 22:04:08 -0400

    change the nav icon color

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00015');
+define('STATIC_VERSION', '00016');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
