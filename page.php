fa68439fdc7870bcf6f654962c2c6ec42885ab56
Nguyễn Lâm <nctl144@gmail.com>
2017-03-31 19:09:20 -0400

    block comment

--- page.php
+++ page.php
@@ -35,16 +35,7 @@ get_header(); ?>
 			    endif;
 			?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php get_template_part( 'page-templates/content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
