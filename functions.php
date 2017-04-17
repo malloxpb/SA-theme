d3c73ab32c38514f11a6a6d3d1d12fc0a7cb4f9e
Nguyễn Lâm <nctl144@gmail.com>
2017-04-17 16:52:56 -0400

    Change static

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00043');
+define('STATIC_VERSION', '00044');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
