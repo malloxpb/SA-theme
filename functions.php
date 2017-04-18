f9f2f3108221302c1a88acefcb702830af9a0513
Nguyễn Lâm <nctl144@gmail.com>
2017-04-18 00:29:30 -0400

    Style the pagination slider

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00046');
+define('STATIC_VERSION', '00048');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
