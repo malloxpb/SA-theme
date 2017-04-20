a14956cc80f0ef42d11969d30a83c8b0067bcda6
Nguyễn Lâm <nctl144@gmail.com>
2017-04-20 16:10:08 -0400

    Fixed the header color of the pages that are not front page

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00050');
+define('STATIC_VERSION', '00051');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
