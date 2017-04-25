0dd1a04d34aaf27e4451546c589689730dca8e61
Nguyễn Lâm <nctl144@gmail.com>
2017-04-25 16:15:48 -0400

    change button hover style

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00055');
+define('STATIC_VERSION', '00057');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
