4ae47e2273af9c4f5d8760c1c4dfceb15659c3e4
Nguyễn Lâm <nctl144@gmail.com>
2017-05-07 03:37:31 -0400

    fix the content being pushed while loading

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00084');
+define('STATIC_VERSION', '00085');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
