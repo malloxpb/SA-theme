c2b0206aec992780fcda7fdd48ac3aec93674db0
Chau Tung Lam <nctl144@gmail.com>
2017-09-25 01:20:31 -0400

    change static

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00087');
+define('STATIC_VERSION', '00088');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
