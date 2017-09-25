1c39d7a8f4838f913b46282f2846034cad5139ea
Chau Tung Lam <nctl144@gmail.com>
2017-09-25 01:03:16 -0400

    hide the header

--- inc/slider.php
+++ inc/slider.php
@@ -37,82 +37,92 @@ function sydney_slider_template() {
         $slider_subtitle_2  = pll__(get_theme_mod('slider_subtitle_2', 'Click the button below'));
         $slider_subtitle_3  = pll__(get_theme_mod('slider_subtitle_3'));
         $slider_subtitle_4  = pll__(get_theme_mod('slider_subtitle_4'));
-        $slider_subtitle_5  = pll__(get_theme_mod('slider_subtitle_5'));  
+        $slider_subtitle_5  = pll__(get_theme_mod('slider_subtitle_5'));
     }
 
     ?>
 
     <div id="slideshow" class="header-slider" data-speed="<?php echo esc_attr($speed); ?>">
         <div class="slides-container">
-            <?php 
+            <?php
                 if ( get_theme_mod('slider_image_1', get_template_directory_uri() . '/static/images/1.png') ) {
                     echo '<div class="slide-item" style="background-image:url(' . esc_url(get_theme_mod('slider_image_1', get_template_directory_uri() . '/static/images/1.jpg')) . ');">';
                     ?>
                         <div class="slide-inner">
                             <div class="contain animated fadeInRightBig text-slider">
+                                <?php /*
                                 <h2 class="maintitle"><?php echo esc_html($slider_title_1); ?></h2>
                                 <p class="subtitle"><?php echo esc_html($slider_subtitle_1); ?></p>
+                                */ ?>
                             </div>
                             <?php sydney_slider_button(); ?>
                         </div>
                     <?php
                     echo '</div>';
-                
+
                 }
                 if ( get_theme_mod('slider_image_2', get_template_directory_uri() . '/images/2.jpg') ) {
                     echo '<div class="slide-item" style="background-image:url(' . esc_url(get_theme_mod('slider_image_2', get_template_directory_uri() . '/static/images/2.jpg')) . ');">';
                     ?>
                         <div class="slide-inner">
                             <div class="contain animated fadeInRightBig text-slider">
+                                <?php /*
                                 <h2 class="maintitle"><?php echo esc_html($slider_title_2); ?></h2>
                                 <p class="subtitle"><?php echo esc_html($slider_subtitle_2); ?></p>
+                                */ ?>
                             </div>
-                            <?php sydney_slider_button(); ?> 
-                        </div>                   
+                            <?php sydney_slider_button(); ?>
+                        </div>
                     <?php
                     echo '</div>';
-                }           
+                }
                 if ( get_theme_mod('slider_image_3') ) {
                     echo '<div class="slide-item" style="background-image:url(' . esc_url(get_theme_mod('slider_image_3')) . ');">';
                     ?>
-                        <div class="slide-inner">                    
+                        <div class="slide-inner">
                             <div class="contain animated fadeInRightBig text-slider">
+                                <?php /*
                                 <h2 class="maintitle"><?php echo esc_html($slider_title_3); ?></h2>
                                 <p class="subtitle"><?php echo esc_html($slider_subtitle_3); ?></p>
+                                */ ?>
                             </div>
                             <?php sydney_slider_button(); ?>
-                        </div>                         
-                    <?php                    
+                        </div>
+                    <?php
                     echo '</div>';
                 }
                 if ( get_theme_mod('slider_image_4') ) {
                     echo '<div class="slide-item" style="background-image:url(' . esc_url(get_theme_mod('slider_image_4')) . ');">';
                     ?>
-                        <div class="slide-inner">                                        
+                        <div class="slide-inner">
                             <div class="contain animated fadeInRightBig text-slider">
+                                <?php /*
                                 <h2 class="maintitle"><?php echo esc_html($slider_title_4); ?></h2>
                                 <p class="subtitle"><?php echo esc_html($slider_subtitle_4); ?></p>
+                                */ ?>
                             </div>
                             <?php sydney_slider_button(); ?>
-                        </div>                        
-                    <?php                    
+                        </div>
+                    <?php
                     echo '</div>';
                 }
                 if ( get_theme_mod('slider_image_5') ) {
                     echo '<div class="slide-item" style="background-image:url(' . esc_url(get_theme_mod('slider_image_5')) . ');">';
                     ?>
-                        <div class="slide-inner">                                                            
+                        <div class="slide-inner">
                             <div class="contain animated fadeInRightBig text-slider">
+                                <?php /*
                                 <h2 class="maintitle"><?php echo esc_html($slider_title_5); ?></h2>
                                 <p class="subtitle"><?php echo esc_html($slider_subtitle_5); ?></p>
+                                */ ?>
                             </div>
-                            <?php sydney_slider_button(); ?> 
-                        </div>                                              
-                    <?php                    
+                            <?php sydney_slider_button(); ?>
+                        </div>
+                    <?php
                     echo '</div>';
-                }               
-            ?>  
-        </div>  
+                }
+            ?>
+        </div>
         <?php if ( $text_slide ) : ?>
             <?php echo sydney_stop_text(); ?>
         <?php endif; ?>
@@ -131,7 +141,7 @@ function sydney_slider_button() {
 
     if ( !function_exists('pll_register_string') ) {
         $slider_button      = get_theme_mod('slider_button_text', 'Click to begin');
-        $slider_button_url  = get_theme_mod('slider_button_url','#primary');        
+        $slider_button_url  = get_theme_mod('slider_button_url','#primary');
     } else {
         $slider_button      = pll__(get_theme_mod('slider_button_text', 'Click to begin'));
         $slider_button_url  = pll__(get_theme_mod('slider_button_url','#primary'));
@@ -149,16 +159,16 @@ function sydney_stop_text() {
         $slider_subtitle_1  = get_theme_mod('slider_subtitle_1','Feel free to look around');
     } else {
         $slider_title_1     = pll__(get_theme_mod('slider_title_1', 'Welcome to Sydney'));
-        $slider_subtitle_1  = pll__(get_theme_mod('slider_subtitle_1','Feel free to look around')); 
+        $slider_subtitle_1  = pll__(get_theme_mod('slider_subtitle_1','Feel free to look around'));
     }
 
-    ?>    
+    ?>
     <div class="slide-inner text-slider-stopped">
         <div class="contain text-slider">
             <h2 class="maintitle"><?php echo esc_html($slider_title_1); ?></h2>
             <p class="subtitle"><?php echo esc_html($slider_subtitle_1); ?></p>
         </div>
         <?php sydney_slider_button(); ?>
-    </div>   
-    <?php 
+    </div>
+    <?php
 }
