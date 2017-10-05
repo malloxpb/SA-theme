9d8b7901c326d094c92ba8a24cf6aa9be718dc49
Chau Tung Lam <nctl144@gmail.com>
2017-10-05 17:24:02 -0400

    change hover color

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00095');
+define('STATIC_VERSION', '00096');
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
