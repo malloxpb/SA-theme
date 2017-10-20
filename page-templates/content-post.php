907469e4ed15983a34db707bd33cea09ea4f7c63
Chau Tung Lam <nctl144@gmail.com>
2017-10-20 00:05:40 -0400

    change the article box style

--- page-templates/content-post.php
+++ page-templates/content-post.php
@@ -4,44 +4,30 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="grid-container poster-inner">
-		<div class="grid-33">
-			<?php if ( has_post_thumbnail() && ( get_theme_mod( 'index_feat_image' ) != 1 ) ) : ?>
-				<div class="poster-thumb">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('sydney-post-thumb'); ?></a>
-				</div>
-			<?php endif; ?>
-		</div>
+<article id="post-<?php the_ID(); ?>" <?php post_class("demo-card-wide mdl-card mdl-shadow--2dp"); ?>>
+	<div class="mdl-card__title" style="background:url('<?php if (has_post_thumbnail() && (get_theme_mod('index_feat_image') != 1)) { the_post_thumbnail_url('sydney-post-thumb'); } ?>')">
+	    <?php the_title( sprintf( '<h2 class="poster-title mdl-card__title-text"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</div>
 
-		<div class="<?php if (has_post_thumbnail()) echo "grid-66"; ?>">
-			<div class="inner-content">
-				<header class="poster-header">
-					<?php the_title( sprintf( '<h2 class="poster-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-				</header><!-- .poster-header -->
+	<div class="mdl-card__supporting-text">
+		<?php if ( (get_theme_mod('full_content_home') == 1 && is_home() ) || (get_theme_mod('full_content_archives') == 1 && is_archive() ) ) : ?>
+			<?php the_content(); ?>
+		<?php else : ?>
+			<?php the_excerpt(); ?>
+		<?php endif; ?>
 
-				<div class="poster-content">
-					<?php if ( (get_theme_mod('full_content_home') == 1 && is_home() ) || (get_theme_mod('full_content_archives') == 1 && is_archive() ) ) : ?>
-						<?php the_content(); ?>
-					<?php else : ?>
-						<?php the_excerpt(); ?>
-					<?php endif; ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sydney' ),
+				'after'  => '</div>',
+			) );
+		?>
+		<?php sydney_posted_on(); ?>
+	</div>
 
-					<?php
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sydney' ),
-							'after'  => '</div>',
-						) );
-					?>
-				</div><!-- .poster-content -->
-			</div>
-
-			<?php if ( 'post' == get_post_type() && get_theme_mod('hide_meta_index') != 1 ) : ?>
-			<div class="poster-meta list-meta clearfix">
-				<?php sydney_posted_on(); ?>
-				<a class="read-more" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php _e('Continue reading','sydney'); ?></a>
-			</div><!-- .poster-meta -->
-			<?php endif; ?>
-		</div>
+	<div class="mdl-card__actions mdl-card--border">
+	    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+	        <?php _e('Continue reading','sydney'); ?>
+	    </a>
 	</div>
 </article><!-- #post-## -->
