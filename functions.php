e0d90a6972ce061694825dcd24bec53a3754ed58
Chau Tung Lam <nctl144@gmail.com>
2017-10-26 14:50:02 -0400

    hide admin bar

--- functions.php
+++ functions.php
@@ -5,7 +5,9 @@
  * @package Sydney
  */
 
-define('STATIC_VERSION', '00104');
+define('STATIC_VERSION', '00109');
+
+show_admin_bar(false);
 
 require get_template_directory() . '/inc/init.php';
 require get_template_directory() . '/inc/action-filters.php';
